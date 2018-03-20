8e3131d72ce2ccd25b562aa38b7c7dcb004005c9
BRYVOGEL <bryvogel@gmail.com>
2018-03-20 15:21:36 -0400

    hover mvp done

--- js/pdp-images_9.js
+++ js/pdp-images_9.js
@@ -146,13 +146,56 @@ $(document).ready(function() {
 
     /***************** Zoom ******************/
 
-    function zoom(e) {
-      var zoomer = e.currentTarget;
-      e.offsetX ? (offsetX = e.offsetX) : (offsetX = e.touches[0].pageX);
-      e.offsetY ? (offsetY = e.offsetY) : (offsetX = e.touches[0].pageX);
-      x = offsetX / zoomer.offsetWidth * 100;
-      y = offsetY / zoomer.offsetHeight * 100;
-      zoomer.style.backgroundPosition = x + "% " + y + "%";
-    }
+    (function() {
+      // Get all images with the `detail-view` class
+      var zoomBoxes = document.querySelectorAll('.detail-view');
+
+      // Extract the URL
+      zoomBoxes.forEach(function(image) {
+        var imageCss = window.getComputedStyle(image, false),
+          imageUrl = imageCss.backgroundImage
+                             .slice(4, -1).replace(/['"]/g, '');
+
+        // Get the original source image
+        var imageSrc = new Image();
+        imageSrc.onload = function() {
+          var imageWidth = imageSrc.naturalWidth,
+              imageHeight = imageSrc.naturalHeight,
+              ratio = imageHeight / imageWidth;
+
+          // Adjust the box to fit the image and to adapt responsively
+          var percentage = ratio * 100 + '%';
+          image.style.paddingBottom = percentage;
+
+          // Zoom and scan on mousemove
+          image.onmousemove = function(e) {
+            // Get the width of the thumbnail
+            var boxWidth = image.clientWidth,
+                // Get the cursor position, minus element offset
+                x = e.pageX - this.offsetLeft,
+                y = e.pageY - this.offsetTop,
+                // Convert coordinates to % of elem. width & height
+                xPercent = x / (boxWidth / 80) + '%',
+                yPercent = y / (boxWidth * ratio / 80) + '%';
+
+            // Update styles w/actual size
+            Object.assign(image.style, {
+              backgroundPosition: xPercent + ' ' + yPercent,
+              backgroundSize: imageWidth + 'px'
+            });
+          };
+
+          // Reset when mouse leaves
+          image.onmouseleave = function(e) {
+            Object.assign(image.style, {
+              backgroundPosition: 'center',
+              backgroundSize: 'cover'
+            });
+          };
+        }
+        imageSrc.src = imageUrl;
+      });
+    })();
+
 
   });
